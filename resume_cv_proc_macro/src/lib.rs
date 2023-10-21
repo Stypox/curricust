1560e9ffaf582bbf6c0eb106bb9196b3e8c7ea15
Stypox <stypox@pm.me>
2023-10-21 08:30:13 +0200

    Use let else

--- resume_cv_proc_macro/src/lib.rs
+++ resume_cv_proc_macro/src/lib.rs
@@ -25,120 +25,119 @@ pub fn derive_cv_element_builder(input: proc_macro::TokenStream) -> proc_macro::
         }
     };
 
-    if let syn::Data::Struct(DataStruct {
+    let syn::Data::Struct(DataStruct {
         fields: Fields::Named(ref fields),
         ..
-    }) = &input.data
-    {
-        let recurse_fields = parse_fields(&fields.named)
-            .chain(once(dummy_string_field("id")))
-            .map(|f| {
-                let MyField { field_name, ty, is_optional: _, has_text_with_attributes_attr, span } = f;
-                if has_text_with_attributes_attr {
-                    quote_spanned! {span=>
-                        #field_name: std::vec::Vec<crate::attr::text_with_attributes::TextWithAttributes>,
-                    }
-                } else {
-                    quote_spanned! {span=>
-                        #field_name: std::option::Option<#ty>,
-                    }
+    }) = &input.data else {
+        unimplemented!();
+    };
+
+    let recurse_fields = parse_fields(&fields.named)
+        .chain(once(dummy_string_field("id")))
+        .map(|f| {
+            let MyField { field_name, ty, is_optional: _, has_text_with_attributes_attr, span } = f;
+            if has_text_with_attributes_attr {
+                quote_spanned! {span=>
+                    #field_name: std::vec::Vec<crate::attr::text_with_attributes::TextWithAttributes>,
                 }
-            });
-
-        let recurse_functions = parse_fields(&fields.named)
-            .chain(once(dummy_string_field("id")))
-            .map(|f| {
-                let MyField { field_name, ty, is_optional: _, has_text_with_attributes_attr, span } = f;
-
-                if has_text_with_attributes_attr {
-                    let fun_name = Ident::new(("add_".to_string() + &field_name.to_string()).as_str(), field_name.span());
-                    quote_spanned! {span=>
-                        pub fn #fun_name(&mut self, e: crate::attr::text_with_attributes::TextWithAttributes) -> &mut Self {
-                            self.#field_name.push(e);
-                            self
-                        }
-                    }
-                } else {
-                    quote_spanned! {span=>
-                        pub fn #field_name(&mut self, e: #ty) -> &mut Self {
-                            self.#field_name = Some(e);
-                            self
-                        }
-                    }
-                }
-            });
-
-        let recurse_build = parse_fields(&fields.named)
-            .map(|f| {
-                let MyField { field_name, ty: _, is_optional, has_text_with_attributes_attr, span } = f;
-                let field_name_error = format!("Missing {field_name}");
-
-                match (has_text_with_attributes_attr, is_optional) {
-                    (false, false) => {
-                        quote_spanned! {span=>
-                            #field_name: self.#field_name.ok_or(#field_name_error)?,
-                        }
-                    },
-                    (false, true) => {
-                        quote_spanned! {span=>
-                            #field_name: self.#field_name,
-                        }
-                    },
-                    (true, false) => {
-                        quote_spanned! {span=>
-                            #field_name: crate::attr::text_with_attributes::TextWithAttributesCollection::into_best_matching(self.#field_name, &active_attrs)
-                                .ok_or(#field_name_error)?,
-                        }
-                    },
-                    (true, true) => {
-                        quote_spanned! {span=>
-                            #field_name: crate::attr::text_with_attributes::TextWithAttributesCollection::into_best_matching(self.#field_name, &active_attrs),
-                        }
-                    },
-                }
-            });
-
-        let recurse_constructor = parse_fields(&fields.named)
-            .chain(once(dummy_string_field("id")))
-            .map(|f| {
-                let field_name = f.field_name;
-                if f.has_text_with_attributes_attr {
-                    quote_spanned! {f.span=>
-                        #field_name: std::vec::Vec::new(),
-                    }
-                } else {
-                    quote_spanned! {f.span=>
-                        #field_name: std::option::Option::None,
-                    }
-                }
-            });
-
-        quote! {
-            #visibility struct #builder_name #ty_generics #where_clause {
-                #(#recurse_fields)*
-            }
-
-            impl #impl_generics #builder_name #ty_generics #where_clause {
-                #(#recurse_functions)*
-
-                pub fn build(self, ctx: &crate::attr::context::Context) -> std::result::Result<#name #name_generics, std::string::String> {
-                    let active_attrs = ctx.get_active_attrs(self.id);
-
-                    std::result::Result::Ok(#name #name_generics {
-                        #(#recurse_build)*
-                    })
+            } else {
+                quote_spanned! {span=>
+                    #field_name: std::option::Option<#ty>,
                 }
             }
+        });
 
-            impl #impl_generics #name #ty_generics #where_clause {
-                #visibility fn builder() -> #builder_name #name_generics {
-                    #builder_name #name_generics {
-                        #(#recurse_constructor)*
+    let recurse_functions = parse_fields(&fields.named)
+        .chain(once(dummy_string_field("id")))
+        .map(|f| {
+            let MyField { field_name, ty, is_optional: _, has_text_with_attributes_attr, span } = f;
+
+            if has_text_with_attributes_attr {
+                let fun_name = Ident::new(("add_".to_string() + &field_name.to_string()).as_str(), field_name.span());
+                quote_spanned! {span=>
+                    pub fn #fun_name(&mut self, e: crate::attr::text_with_attributes::TextWithAttributes) -> &mut Self {
+                        self.#field_name.push(e);
+                        self
+                    }
+                }
+            } else {
+                quote_spanned! {span=>
+                    pub fn #field_name(&mut self, e: #ty) -> &mut Self {
+                        self.#field_name = Some(e);
+                        self
                     }
                 }
             }
-        }.into()
-    } else {
-        unimplemented!()
-    }
+        });
+
+    let recurse_build = parse_fields(&fields.named)
+        .map(|f| {
+            let MyField { field_name, ty: _, is_optional, has_text_with_attributes_attr, span } = f;
+            let field_name_error = format!("Missing {field_name}");
+
+            match (has_text_with_attributes_attr, is_optional) {
+                (false, false) => {
+                    quote_spanned! {span=>
+                        #field_name: self.#field_name.ok_or(#field_name_error)?,
+                    }
+                },
+                (false, true) => {
+                    quote_spanned! {span=>
+                        #field_name: self.#field_name,
+                    }
+                },
+                (true, false) => {
+                    quote_spanned! {span=>
+                        #field_name: crate::attr::text_with_attributes::TextWithAttributesCollection::into_best_matching(self.#field_name, &active_attrs)
+                            .ok_or(#field_name_error)?,
+                    }
+                },
+                (true, true) => {
+                    quote_spanned! {span=>
+                        #field_name: crate::attr::text_with_attributes::TextWithAttributesCollection::into_best_matching(self.#field_name, &active_attrs),
+                    }
+                },
+            }
+        });
+
+    let recurse_constructor = parse_fields(&fields.named)
+        .chain(once(dummy_string_field("id")))
+        .map(|f| {
+            let field_name = f.field_name;
+            if f.has_text_with_attributes_attr {
+                quote_spanned! {f.span=>
+                    #field_name: std::vec::Vec::new(),
+                }
+            } else {
+                quote_spanned! {f.span=>
+                    #field_name: std::option::Option::None,
+                }
+            }
+        });
+
+    quote! {
+        #visibility struct #builder_name #ty_generics #where_clause {
+            #(#recurse_fields)*
+        }
+
+        impl #impl_generics #builder_name #ty_generics #where_clause {
+            #(#recurse_functions)*
+
+            pub fn build(self, ctx: &crate::attr::context::Context) -> std::result::Result<#name #name_generics, std::string::String> {
+                let active_attrs = ctx.get_active_attrs(self.id);
+
+                std::result::Result::Ok(#name #name_generics {
+                    #(#recurse_build)*
+                })
+            }
+        }
+
+        impl #impl_generics #name #ty_generics #where_clause {
+            #visibility fn builder() -> #builder_name #name_generics {
+                #builder_name #name_generics {
+                    #(#recurse_constructor)*
+                }
+            }
+        }
+    }.into()
 }
